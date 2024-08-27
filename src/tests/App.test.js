b3545a9bd2d674eaf2885b0a2c912e90b84ccbd1
Tejshree-bang <tejshreebang07.23@gmail.com>
2024-08-27 21:43:30 +0530

    Changes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -7,9 +7,9 @@ jest.mock("../utils/fetchTransactions");
 
 // Mock data representing the transactions that the fetchTransactions function will return.
 const mockTransactions = [
-  { customerId: 1, name: "John Doe", mobile: "123-456-7890", date: "2024-01-15", amount: 120 },
-  { customerId: 1, name: "John Doe", mobile: "123-456-7890", date: "2024-02-10", amount: 75 },
-  { customerId: 2, name: "Jane Smith", mobile: "098-765-4321", date: "2024-02-15", amount: 150 },
+  { customerId: 1, name: "John Doe", mobile: "1234567890", date: "2024-01-15", amount: 120 },
+  { customerId: 1, name: "John Doe", mobile: "1234567890", date: "2024-02-10", amount: 75 },
+  { customerId: 2, name: "Jane Smith", mobile: "0987654321", date: "2024-02-15", amount: 150 },
 ];
 
 // Test to check if the loading state is rendered initially when the App component is mounted.
@@ -29,13 +29,10 @@ test("renders customer points after data is loaded", async () => {
   // Mock the fetchTransactions function to return the mockTransactions data.
   fetchUtils.fetchTransactions.mockResolvedValueOnce(mockTransactions);
 
-  // Render the App component.
   render(<App />);
 
   // Wait for the text "John Doe" to be found in the document after loading the data.
   expect(await screen.findByText(/John Doe/i)).toBeInTheDocument();
-
-  // Wait for the text "Jane Smith" to be found in the document after loading the data.
   expect(await screen.findByText(/Jane Smith/i)).toBeInTheDocument();
 });
 
@@ -44,7 +41,6 @@ test("filters customers based on search input", async () => {
   // Mock the fetchTransactions function to return the mockTransactions data.
   fetchUtils.fetchTransactions.mockResolvedValueOnce(mockTransactions);
 
-  // Render the App component.
   render(<App />);
 
   // Find the search input field by its placeholder text.
